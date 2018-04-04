9447e2b45529747f8537c68785686eaf6061327d
cmbahadir <c.mete.bahadir@gmail.com>
2018-04-04 10:17:27 +0300

    Update add.php
    Deleted commented out codes.

--- add.php
+++ add.php
@@ -3,36 +3,6 @@
    	
    	$link=Connection();
 
-	
-
-	/*$hex1 = isset($_POST['hex1']) ? $_POST['hex1'] : '';
-	$hex1 = !empty($_POST['hex1']) ? $_POST['hex1'] : '';
-	
-	$flight1 = isset($_POST['flight1']) ? $_POST['flight1'] : '';
-	$flight1 = !empty($_POST['flight1']) ? $_POST['flight1'] : '';
-	
-	$altitude1 = isset($_POST['altitude1']) ? $_POST['altitude1'] : '';
-	$altitude1 = !empty($_POST['altitude1']) ? $_POST['altitude1'] : '';
-	
-	$speed1 = isset($_POST['speed1']) ? $_POST['speed1'] : '';
-	$speed1 = !empty($_POST['speed1']) ? $_POST['speed1'] : '';
-	
-	$lat1 = isset($_POST['lat1']) ? $_POST['lat1'] : '';
-	$lat1 = !empty($_POST['lat1']) ? $_POST['lat1'] : '';
-	
-	$lon1 = isset($_POST['lon1']) ? $_POST['lon1'] : '';
-	$lon1 = !empty($_POST['lon1']) ? $_POST['lon1'] : '';
-	
-	$track1 = isset($_POST['track1']) ? $_POST['track1'] : '';
-	$track1 = !empty($_POST['track1']) ? $_POST['track1'] : '';
-	
-	$messages1 = isset($_POST['messages1']) ? $_POST['messages1'] : '';
-	$messages1 = !empty($_POST['messages1']) ? $_POST['messages1'] : '';
-	
-	$seen1 = isset($_POST['seen1']) ? $_POST['seen1'] : '';
-	$seen1 = !empty($_POST['seen1']) ? $_POST['seen1'] : '';*/
-
-
 	$hex=$_POST["hex"];
 	$squawk=$_POST["squawk"];
 	$flight=$_POST["flight"];
