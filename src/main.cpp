241b42de41be6a5948f991c4e06154f9b35d6ebc
cmbahadir <c.mete.bahadir@gmail.com>
2018-12-07 13:55:23 +0300

    First message in the queue.

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     //AMQP Handlers
-    MyTcpHandler myHandler;
     boost::asio::io_service service(4);
     AMQP::LibBoostAsioHandler handler(service);
 
@@ -20,7 +19,7 @@ int main(int argc, char *argv[]) {
     vector<string> v;
     char comma = ',';
     int hostport = 30003;
-    bool connectStatus;
+    bool connectStatus, queueDeclareStat;
 
     if(argc < 2) {
         cout << "Usage: PlaneSpotter <hostname>" << endl;
@@ -28,12 +27,13 @@ int main(int argc, char *argv[]) {
     }
     else {
         AMQP::TcpConnection connection(&handler, AMQP::Address("amqp://guest:guest@localhost/"));
-        AMQP::TcpChannel channel(&connection);
-        channel.declareExchange("ADSB_EXCHANGE");
-        channel.declareQueue("ADSB_QUEUE").onSuccess([&connection](const std::string &name, uint32_t messagecount, uint32_t consumercount) {
+        AMQP::TcpChannel adsbChannel(&connection);
+        adsbChannel.declareExchange("ADSB_EXCHANGE",AMQP::fanout);
+        adsbChannel.declareQueue("ADSB_QUEUE").onSuccess([&connection](const std::string &name, uint32_t messagecount, uint32_t consumercount) {
             std::cout << "declared queue " << name << std::endl;
             //connection.close();
         });
+        adsbChannel.bindQueue("ADSB_EXCHANGE", "ADSB_QUEUE", "my-key");
         //Class instantiation
         hostaddress = argv[1];
         DecodeSBS* decode = new DecodeSBS();
@@ -41,9 +41,11 @@ int main(int argc, char *argv[]) {
         connectStatus = client->connected;
         if (connectStatus == true){
             string response = client->read();
-            cout << "Response: %s" << response << endl;
+            cout << "Response: " << response << endl;
             //TODO : Publish response
-            //channel.publish()
+            adsbChannel.startTransaction();
+            adsbChannel.publish("ADSB_EXCHANGE", "my-key", response);
+            adsbChannel.commitTransaction();
             decode->split(response, comma, v);
             client->disconnect(); //Disconnnect method is not implemented.
         }
